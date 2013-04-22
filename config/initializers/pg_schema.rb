ac9d1fab87be88fc1e05a1595f498566b4274a95
Hassan Shahid <set5think@gmail.com>
2013-04-22 15:24:59 -0400

    storing schema in DATABASE_URL

--- config/initializers/pg_schema.rb
+++
@@ -1,7 +0,0 @@
-schema_name = 'copycopter'
-
-if !ActiveRecord::Base.connection.schema_exists?(schema_name)
-  ActiveRecord::Base.connection.execute("CREATE SCHEMA #{schema_name}")
-end
-
-ActiveRecord::Base.connection.execute("SET search_path to #{schema_name}")
